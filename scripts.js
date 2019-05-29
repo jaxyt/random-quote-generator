7696d81f53f1cd4dc9c530ba3d293e68a4acb2de
jax <jax@allpurposeit.com>
2019-05-29 11:03:20 -0400

    fully functional

--- scripts.js
+++ scripts.js
@@ -1,34 +1,30 @@
 
 var quotes = [
     {
-    quote: 'hi there', 
-    source: 'jax', 
-    citation: 'wiki', 
-    year: 1768
+        quote: 'Get busy living or get busy dying.', 
+        source: 'Stephen King', 
+        citation: 'Shawshank Redemption', 
+        year: 1994
     },
     {
-    quote: 'hello', 
-    source: 'adriel', 
-    citation: 'wiki', 
-    year: 1942
+        quote: 'Great minds discuss ideas; average minds discuss events; small minds discuss people.', 
+        source: 'Eleanor Roosevelt',  
+        year: 1987
     },
     {
-    quote: 'shut up', 
-    source: 'olivia', 
-    citation: 'wiki', 
-    year: 800
+        quote: 'Those who dare to fail miserably can achieve greatly.', 
+        source: 'John F. Kennedy', 
+        citation: 'US Inaugural Address', 
+        year: 1989
     },
     {
-    quote: 'dope sauce', 
-    source: 'anon', 
-    citation: 'wiki', 
-    year: 1776
+        quote: 'It is hard to fail, but it is worse never to have tried to succeed.', 
+        source: 'Theodore Roosevelt', 
     },
     {
-    quote: 'beans and cornbread', 
-    source: 'jb smoove', 
-    citation: 'tosh.0', 
-    year: 2019
+        quote: 'Every man loves justice at another man’s expense.', 
+        source: 'Anonymous',
+        citation: 'Forbes'
     },
     {
         quote: 'Humility is no substitute for a good personality.', 
@@ -46,7 +42,15 @@ function getRandomQuote(list) {
 
 function printQuote(quotation) {
     var output = document.getElementById('output');
-    output.innerHTML = '<h1 id="head">“' + quotation.quote + '”</h1>' + '<p class="paragraph">-' + quotation.source + ', <span class="citation">' + quotation.citation + '</span>, ' + quotation.year + '</p>';
+    if (quotation.year && quotation.citation) {
+        output.innerHTML = '<h1 id="head">“' + quotation.quote + '”</h1>' + '<p class="paragraph">-' + quotation.source + ', <span class="citation">' + quotation.citation + '</span>, ' + quotation.year + '</p>';
+    } else if (quotation.year) {
+        output.innerHTML = '<h1 id="head">“' + quotation.quote + '”</h1>' + '<p class="paragraph">-' + quotation.source + ', ' + quotation.year + '</p>';
+    } else if (quotation.citation) {
+        output.innerHTML = '<h1 id="head">“' + quotation.quote + '”</h1>' + '<p class="paragraph">-' + quotation.source + ', <span class="citation">' + quotation.citation + '</span></p>';
+    } else {
+        output.innerHTML = '<h1 id="head">“' + quotation.quote + '”</h1>' + '<p class="paragraph">-' + quotation.source +'</p>';
+    }
 }
 
 
